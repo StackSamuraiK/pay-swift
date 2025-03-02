a3c1acf269c05d056246a171d6361e12d238e78b
StackSamuraiK <kshitizsrivastav3344@gmail.com>
2025-03-02 15:26:27 +0530

    changed app/page.tsx

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -2,11 +2,14 @@
 
 export default function Home() {
   return (
+    <div>
+
     <div className="font-semibold bg-red-950 text-6xl">
       Helooooooo
     </div>
     <div>
       Heloooo
+    </div>
     </div>
   );
 }
